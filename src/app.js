9eadffc73f82ff4973917c47463202089d61ed74
Tiago da Silva <tiagopan101@gmail.com>
2023-02-08 23:26:35 -0400

    alterando config helmet

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ class App {
 
   middlewares () {
     this.app.use(cors(corsOptions))
-    this.app.use(helmet.crossOriginResourcePolicy({ policy: false }))
+    this.app.use(helmet({ permittedCrossDomainPolicies: true }))
     this.app.use(express.urlencoded({ extended: true }))
     this.app.use(express.json())
     this.app.use(express.static(resolve(__dirname, '..', 'uploads')))
