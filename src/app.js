d7065622f1d8ea5ff7ac6414aae6e37ada6abc52
Tiago da Silva <tiagopan101@gmail.com>
2023-03-02 21:32:06 -0400

    cors

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ class App {
   }
 
   middlewares () {
-    this.app.use(cors(corsOptions))
+    this.app.use(cors())
     this.app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }))
     this.app.use(express.urlencoded({ extended: true }))
     this.app.use(express.json())
