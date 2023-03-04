cf515aa68cfb1f40f4b668140061f0f140e306d4
Tiago da Silva <tiagopan101@gmail.com>
2023-03-03 23:20:48 -0400

    axios

--- dist/app.js
+++ dist/app.js
@@ -41,7 +41,7 @@ class App {
 
   middlewares () {
     this.app.use(_cors2.default.call(void 0, corsOptions))
-    this.app.use(_helmet2.default.call(void 0, { crossOriginResourcePolicy: { policy: 'cross-origin' } }))
+    this.app.use(_helmet2.default.call(void 0, { crossOriginResourcePolicy: { policy: 'cross-origin' }, referrerPolicy: { policy: 'origin-when-cross-origin' } }))
     this.app.use(_express2.default.urlencoded({ extended: true }))
     this.app.use(_express2.default.json())
     this.app.use(_express2.default.static(_path.resolve.call(void 0, __dirname, '..', 'uploads')))
--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ class App {
 
   middlewares () {
     this.app.use(cors(corsOptions))
-    this.app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }))
+    this.app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' }, referrerPolicy: { policy: 'origin-when-cross-origin' } }))
     this.app.use(express.urlencoded({ extended: true }))
     this.app.use(express.json())
     this.app.use(express.static(resolve(__dirname, '..', 'uploads')))
