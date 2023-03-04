8f1c9ed3e078d9433a81ec5303d7a753e1b142b5
Tiago da Silva <tiagopan101@gmail.com>
2023-03-03 23:59:01 -0400

    axios

--- dist/app.js
+++ dist/app.js
@@ -40,7 +40,7 @@ class App {
   }
 
   middlewares () {
-    this.app.use(_cors2.default.call(void 0, 'http:localhost:3000/'))
+    this.app.use(_cors2.default.call(void 0, { origin: 'http:localhost:3000/' }))
     this.app.use(_helmet2.default.call(void 0, { crossOriginResourcePolicy: { policy: 'cross-origin' } }))
     this.app.use(_express2.default.urlencoded({ extended: true }))
     this.app.use(_express2.default.json())
