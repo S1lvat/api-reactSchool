b3ce177143476ff9744937bfd2c5bf3fa9cde342
Tiago da Silva <tiagopan101@gmail.com>
2023-03-08 22:20:46 -0400

    axios

--- src/app.js
+++ src/app.js
@@ -15,19 +15,19 @@ import fotoRoutes from './routes/fotoRoutes'
 
 import './dataBase'
 
-// const whiteList = [
-//   'http://localhost:3000/'
-// ]
+const whiteList = [
+  'http://localhost:3000'
+]
 
-// // const corsOptions = {
-// //   origin: function (origin, callback) {
-// //     if (whiteList.indexOf(origin) === -1 || !origin) {
-// //       callback(null, true)
-// //     } else {
-// //       callback(new Error('Not allowed by CORS!'))
-// //     }
-// //   }
-// // }
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) === -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS!'))
+    }
+  }
+}
 
 class App {
   constructor () {
@@ -37,9 +37,8 @@ class App {
   }
 
   middlewares () {
-    this.app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+    this.app.use(cors(corsOptions))
     this.app.use(helmet({
-      crossOriginResourcePolicy: { policy: 'cross-origin' },
       contentSecurityPolicy: {
         directives: {
           defaultSrc: ['http://localhost:3000']
