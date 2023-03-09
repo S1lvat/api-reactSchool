73d39aba454d072d1077b0898b1594658f94eb39
Tiago da Silva <tiagopan101@gmail.com>
2023-03-08 23:22:44 -0400

    axios

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ class App {
       crossOriginResourcePolicy: { policy: 'cross-origin' },
       contentSecurityPolicy: {
         directives: {
-          'img-src': ['http://localhost:3000']
+          'default-src': ['http://localhost:3000']
         }
       }
     }))
