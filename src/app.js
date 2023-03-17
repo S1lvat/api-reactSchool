be0ed5fa7c1601479d2a223bb3392d1681fb8e7d
Tiago da Silva <tiagopan101@gmail.com>
2023-03-16 21:17:39 -0400

    axios

--- src/app.js
+++ src/app.js
@@ -15,19 +15,20 @@ import fotoRoutes from './routes/fotoRoutes'
 
 import './dataBase'
 
-// const whiteList = [
-//   'http://localhost:3000'
-// ]
+const whiteList = [
+  'http://localhost:3000'
+]
 
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whiteList.indexOf(origin) === -1 || !origin) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS!'))
-//     }
-//   }
-// }
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) === -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS!'))
+    }
+  },
+  optionsSuccessStatus: 200
+}
 
 class App {
   constructor () {
@@ -37,8 +38,11 @@ class App {
   }
 
   middlewares () {
-    this.app.use(cors({ origin: '*' }))
-    this.app.use(helmet())
+    this.app.use(cors(corsOptions))
+    this.app.use(helmet({
+      crossOriginResourcePolicy: { policy: 'cross-origin' }
+
+    }))
     this.app.use(express.urlencoded({ extended: true }))
     this.app.use(express.json())
     this.app.use(express.static(resolve(__dirname, '..', 'uploads')))
