3e22cf20e3f4587cd5b687ca853d66fba2b21603
Tiago da Silva <tiagopan101@gmail.com>
2023-03-30 22:19:22 -0400

    cors

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,4 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _appjs = require('./app.js'); var _appjs2 = _interopRequireDefault(_appjs);
 
-const port = process.env.PORT_SERVER
+const port = 3001
 _appjs2.default.listen(port, () => { console.log(`listen on port: ${port}`) })
--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
 import app from './app.js'
 
-const port = process.env.PORT_SERVER
+const port = 3001
 app.listen(port, () => { console.log(`listen on port: ${port}`) })
